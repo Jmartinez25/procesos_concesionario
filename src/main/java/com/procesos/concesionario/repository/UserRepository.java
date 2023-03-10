87a021852e48f0621fed94af2399e5d0999c1ce0
Juan Diego Martinez <mjuanb25@gmail.com>
2023-03-09 20:50:38 -0500

    Creacion arquitectura y base de datos

---
+++ src/main/java/com/procesos/concesionario/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.procesos.concesionario.repository;
+
+import com.procesos.concesionario.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
+
